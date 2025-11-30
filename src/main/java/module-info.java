e3c42785ae6e6d4e2154e0b26a26b8ba07513103
joaovbeutler <jvbeutler70@gmail.com>
2025-11-30 19:08:00 -0300

    First functional version of the Pokédex

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module br.unijui.edu.prog.avaliacao.pokemonapp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.net.http;
+    requires org.json;
 
     opens br.unijui.edu.prog.avaliacao.pokemonapp to javafx.fxml;
     exports br.unijui.edu.prog.avaliacao.pokemonapp;
