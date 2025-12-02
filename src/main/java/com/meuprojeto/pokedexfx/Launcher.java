0000e41cedc07f5a8e9a2a1d48a9e32154795e80
Vítor L Klock <klockvitor@gmail.com>
2025-12-02 09:11:22 -0300

    working again

--- src/main/java/com/meuprojeto/pokedexfx/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.meuprojeto.pokedexfx;
-
-public class Launcher {
-    
-    public static void main(String[] args) {
-        // Este método apenas chama o main da sua classe App
-        App.main(args);
-    }
-}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,12 @@
-module br.unijui.edu.prog.avaliacao.pokemonapp {
+module com.meuprojeto.pokedexfx {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.google.gson;
     requires java.net.http;
     requires jdk.crypto.ec;
 
-    opens com.meuprojeto.pokedexfx to javafx.fxml;
     exports com.meuprojeto.pokedexfx;
+    
+    // "opens" permite que bibliotecas externas (Gson e JavaFX) mexam nas suas classes via reflexão
+    opens com.meuprojeto.pokedexfx to com.google.gson, javafx.fxml;
 }
